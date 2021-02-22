ca0328f6d5debd24df4b339b70c1a9979aeaa8db
Katerina <77492568+Vampire1K@users.noreply.github.com>
2021-02-22 17:56:32 +0530

    Add files via upload

---
+++ baseclass.js
@@ -0,0 +1,29 @@
+class BaseClass {
+    constructor (x,y,width,height){
+        var options={
+            restitution:0.8,
+            friction : 1.0,
+            density : 1.0
+        }  
+        // box1= new Box(100,200,20,20);
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage('sprites/base.png');
+        World.add(world,this.body);
+
+    }
+
+    display(){
+        push ();
+
+        translate(this.body.position.x,this.body.position.y);
+        angleMode(RADIANS);
+        rotate(this.body.angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    
+    }
+    
+}
--- box.js
+++ box.js
@@ -1,27 +1,7 @@
-class Box {
-    constructor (x,y,width,height){
-        var options={
-            restitution:0.8
-        }  
-        // box1= new Box(100,200,20,20);
-        this.body = Bodies.rectangle(x,y,width,height,options);
-        this.width = width;
-        this.height = height;
-        World.add(world,this.body);
-
-    }
-    display(){
-        push ();
-
-        translate(this.body.position.x,this.body.position.y);
-        angleMode(RADIANS);
-        rotate(this.body.angle);
-        fill("blue");
-        // rect(100,50,10,80);
-        rectMode(CENTER);
-        rect(0,0,this.width,this.height)
-        pop();
-    
+class Box extends BaseClass {
+    constructor(x,y,width,height){
+        super(x,y,width,height);
+        this.image = loadImage('sprites/wood1.png');
     }
     
 }
